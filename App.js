90608e53648854db64cfc06a3092950c8ce44212
AndreyMB <andreybonanno2@gmail.com>
2023-10-11 21:50:04 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from './screens/Login';
+import CreateUser from './screens/CreateUser';
+import FeedBooks from './screens/FeedBooks';
+import DetailBook from './screens/DetailBook';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "LOGIN" }}
+        />
+
+<Stack.Screen
+          name="CreateUser"
+          component={CreateUser}
+          options={{ title: "CADASTRO" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
+
+}
